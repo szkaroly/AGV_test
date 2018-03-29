0d4c2dcc252e703d2b4d1648778adcda8f4c0ed7
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-03-29 19:12:18 +0200

    ReferenceInput fixed + test added

--- python/vrep/helloworld/BezierUtils.py
+++ python/vrep/helloworld/BezierUtils.py
@@ -1,16 +1,20 @@
 import numpy as np
 from math import cos, sin, atan2
-import pandas as pd
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 
-''' P1 : initial position of the desired path
+'''
+P1 : initial position of the desired path
     P2 : initial tangent of the desired path
     P3 : final position of the desired path
     P4 : final tangent of the desired path
     SamplingTime : timestep in seconds
-    EndTime : total simulation time''' # Checked
+    EndTime : total simulation time
+''' # Checked
 def generateBezier(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
-                   SamplingTime: float, EndTime: float):
+                   SamplingTime: float, EndTime: float) -> np.array:
     t = np.arange(start = 0, stop = EndTime + 2 * SamplingTime, step = SamplingTime)
     t = t / t[-1] # ok so far
 
@@ -26,37 +30,43 @@ def generateBezier(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
     # it is needed to avoid decreasing the number of samples)
     return np.vstack([ReferencePosition[0], ReferencePosition[1], ReferenceOrientation])
 
-
-def generateReferenceInput(ReferenceTrajectory, SamplingTime):
-    XLIMIT = 0.00001
+'''
+This function generates the reference velocity input (v, omega) for the vehicle
+ReferenceTrajectory : np.array  -> containing x,y, orientation values
+SamplingTime        : float     -> timestep
+XLIMIT              : flaot     -> ???
+'''
+def generateReferenceInput(ReferenceTrajectory : np.array, SamplingTime : float , XLIMIT = 0.00001) -> np.array:
     N = len(ReferenceTrajectory[1])
-
+    reference_input = np.empty([2,N-1])
     for ii in range(N - 1):
-        if ii == 1:
-            phik = ReferenceTrajectory[1][ii]
+        if ii == 0:
+            phik = ReferenceTrajectory[(2,ii)]
         else:
-            phik = ReferenceTrajectory[1][ii - 1]
-    xk = ReferenceTrajectory[0][0][ii];  # x,ref,k spatial
-    yk = ReferenceTrajectory[0][1][ii];  # y,ref,k spatial
-    xs = ReferenceTrajectory[0][0][ii + 1];  # x,ref,k+1 spatial
-    ys = ReferenceTrajectory[0][1][ii + 1];  # y,ref,k+1 spatial
-    inverz = np.linalg.inv(planarTransform(phik, np.matrix([[xk], [yk]]), 1))
-    DBody = inverz * np.matrix([[xs], [ys], [1]])
-    print('DBODY:\n', DBody)
-    xb = DBody[0]
-    yb = DBody[1]
+            phik = ReferenceTrajectory[(2,ii - 1)]
+        xk = ReferenceTrajectory[(0,ii)]   # x,ref,k spatial
+        yk = ReferenceTrajectory[(1,ii)]    # y,ref,k spatial
+        xs = ReferenceTrajectory[(0,ii+1)]  # x,ref,k+1 spatial
+        ys = ReferenceTrajectory[(1,ii+1)]  # y,ref,k+1 spatial
+        planarT = planarTransform(phik, np.matrix([[xk],[yk]]), 1)
+        inverz = np.linalg.inv(planarT)
+        DBody = inverz * np.matrix([[xs], [ys], [1]])
+        xb = float(DBody[0])
+        yb = float(DBody[1])
 
-    if abs(xb) < XLIMIT:
-        RefVel = (0, 0)
-    else:
-        # FIXME
-        helper = np.matrix([[xb * cos(phik) - yb * sin(phik), xb * sin(phik) + yb * cos(phik)],
-                            [-sin(phik), cos(phik)]])
-        RefVel = 1 / (SamplingTime * xb) * helper * np.matrix([xs - xk, ys - yk])
-        # RefVel = 1/(SamplingTime*xb)*[xb*cos(phik)-yb*sin(phik) xb*sin(phik)+yb*cos(phik);
-        #                               -sin(phik) cos(phik)]*[xs-xk;ys-yk];
+        if abs(xb) < XLIMIT:
+            RefVel = (0, 0)
+        else:
+            helper = np.matrix([[(xb * cos(phik) - yb * sin(phik)), (xb * sin(phik) + yb * cos(phik))],
+                                [-sin(phik)                     , cos(phik)]])
+            RefVel = 1 / (SamplingTime * xb) * helper * np.matrix([[xs - xk], [ys - yk]])
 
-        print(RefVel)
+        reference_input[(0,ii)] = RefVel[0]
+        reference_input[(1,ii)] = RefVel[1]
+
+    return reference_input
+
+
 
 
 # Planar rotation - checked
@@ -85,7 +95,9 @@ if __name__ == "__main__":
             p2 = np.array([[1], [0]])
             p3 = np.array([[1], [1]])
             p4 = np.array([[1], [2]])
-            ReferenceTrajectory = generateBezier(p1, p2, p3, p4, 0.1, 1)
+            dt = 0.1
+            time = 1
+            ReferenceTrajectory = generateBezier(p1, p2, p3, p4, dt, time)
             #print(ReferenceTrajectory)
             self.assertEqual(ReferenceTrajectory[0][0], 0)
             self.assertEqual(ReferenceTrajectory[1][0], 0)
@@ -114,12 +126,19 @@ if __name__ == "__main__":
             self.assertAlmostEqual(M[(2,1)],  0.0000, places = 4)
             self.assertAlmostEqual(M[(2,2)],  1.5000, places = 4)
 
-        def testGenerateReferenceTrajectory(self):
+        def testGenerateReferenceInput(self):
             p1 = np.array([[0], [0]])
             p2 = np.array([[1], [0]])
             p3 = np.array([[1], [1]])
             p4 = np.array([[1], [2]])
-            ReferenceTrajectory = generateBezier(p1, p2, p3, p4, 0.1, 1)
+            dt = 0.1
+            time = 1
+            reference_trajectory = generateBezier(p1, p2, p3, p4, dt, time)
+            reference_input = generateReferenceInput(reference_trajectory, dt)
+            #These values are copied from a matlab result with the same parameters
+            expected = np.array([[2.4984, 2.2088, 2.0361, 2.0013, 2.0646, 2.1867, 2.3326, 2.4742, 2.5928, 2.6769, 2.7202],
+                                 [0.0000, 2.3509, 2.7135, 2.6807, 2.2672, 1.7252, 1.2418, 0.8651, 0.5790, 0.3553, 0.1690]])
+            np.testing.assert_array_almost_equal(reference_input, expected, decimal=4)
 
 
 
