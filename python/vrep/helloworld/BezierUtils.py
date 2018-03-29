23dfb61d7f44f599945dbc838bc53e7b9953ce25
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-03-29 18:12:47 +0200

    Added test for planar rotation & transformation

--- python/vrep/helloworld/BezierUtils.py
+++ python/vrep/helloworld/BezierUtils.py
@@ -8,7 +8,7 @@ import pandas as pd
     P3 : final position of the desired path
     P4 : final tangent of the desired path
     SamplingTime : timestep in seconds
-    EndTime : total simulation time'''
+    EndTime : total simulation time''' # Checked
 def generateBezier(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
                    SamplingTime: float, EndTime: float):
     t = np.arange(start = 0, stop = EndTime + 2 * SamplingTime, step = SamplingTime)
@@ -19,11 +19,7 @@ def generateBezier(P1: np.array, P2: np.array, P3: np.array, P4: np.array,
                         + np.kron(3 * (1 - t) * np.power(t,2) , P3)\
                         + np.kron(    np.power(t, 3)          , P4)
 
-    print('Refpos', ReferencePosition)
-
     diff = np.diff(ReferencePosition)
-    print('\ndiff[0]->',diff[0])
-    print('\ndiff[1]->',diff[1], '\n\n')
     ReferenceOrientation = np.arctan2(diff[1], diff[0])
     ReferenceOrientation = np.append(ReferenceOrientation, ReferenceOrientation[-1])
     # the last two orientations are identical (this is just an approximation, but
@@ -74,9 +70,9 @@ def planarTransform(angle, position, scale):
     # new matrix is constructed via: [2x2 + 2x1]
     #                               [0,  0, 1 ]
     matrix = np.hstack([planarRot(angle), position])
-    matrix = np.vstack([matrix, np.matrix([0, 0, 1])])
+    result = np.vstack([matrix, np.matrix([0, 0, 1])])
     # matrix = np.matrix([planarRot(angle), position, np.matrix([0,0,1])])
-    return scale * matrix
+    return scale * result
 
 
 # TODO : Write tests
@@ -98,17 +94,37 @@ if __name__ == "__main__":
             self.assertAlmostEqual(ReferenceTrajectory[2][0], 0.0964, places = 3)
             self.assertAlmostEqual(ReferenceTrajectory[2][-1], 1.5680, places = 3)
 
+        def testPlanarRotation(self):
+            M1 = planarRot(90)
+            self.assertAlmostEqual(M1[(0,0)], -0.4481, places = 4)
+            self.assertAlmostEqual(M1[(0,1)], -0.8940, places = 4)
+            self.assertAlmostEqual(M1[(1,0)],  0.8940, places = 4)
+            self.assertAlmostEqual(M1[(1,1)], -0.4481, places = 4)
+
+
+        def testPlanarTransform(self):
+            M  = planarTransform(45, np.matrix([[2], [2]]), 1.5)
+            self.assertAlmostEqual(M[(0,0)], 0.7880, places = 4)
+            self.assertAlmostEqual(M[(0,1)], -1.2764, places = 4)
+            self.assertAlmostEqual(M[(0,2)],  3.0000, places = 4)
+            self.assertAlmostEqual(M[(1,0)],  1.2764, places = 4)
+            self.assertAlmostEqual(M[(1,1)],  0.7880, places = 4)
+            self.assertAlmostEqual(M[(1,2)],  3.0000, places = 4)
+            self.assertAlmostEqual(M[(2,0)],  0.0000, places = 4)
+            self.assertAlmostEqual(M[(2,1)],  0.0000, places = 4)
+            self.assertAlmostEqual(M[(2,2)],  1.5000, places = 4)
+
+        def testGenerateReferenceTrajectory(self):
+            p1 = np.array([[0], [0]])
+            p2 = np.array([[1], [0]])
+            p3 = np.array([[1], [1]])
+            p4 = np.array([[1], [2]])
+            ReferenceTrajectory = generateBezier(p1, p2, p3, p4, 0.1, 1)
 
 
 
-            def test2(self):
-                angle = 0
-                position = np.matrix([[1], [1]])
-                print(planarTransform(angle, position, 6))
-
-
-                def test3(self):
-                    pass
+        def test3(self):
+            pass
                     #traj = test1()
                     #generateReferenceInput(traj, 0.01)
 
