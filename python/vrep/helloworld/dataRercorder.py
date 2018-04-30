29594f24a07816bd180d7724e62a391530f5a052
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-04-30 13:28:03 +0200

    data recorder extended with index col

--- python/vrep/helloworld/dataRercorder.py
+++ python/vrep/helloworld/dataRercorder.py
@@ -6,14 +6,28 @@ import time
 
 class dataRecorder(object):
     def __init__(self):
-        self.pos = {'x': [], 'y': [], 'th':[]}
+        self.pos = {'index': [], 'x' : [], 'y': [], 'th':[]}
+        self.posIndex = 0
+        self.vel = {'index': [], 'v' : [], 'w':[]}
+        self.velIndex = 0
 
     def recordPosition(self, x, y,th):
         ts = time.time()
+        self.posIndex = self.posIndex +1
+        self.pos['index'].append(self.posIndex)
         self.pos['x'].append(x)
         self.pos['y'].append(y)
         self.pos['th'].append(th)
 
+    def recordVelocity(self, v, w):
+        self.velIndex = self.velIndex +1
+        self.vel['index'].append(self.velIndex)
+        self.vel['v'].append(v)
+        self.vel['w'].append(w)
+
     def save(self):
         df = pd.DataFrame(self.pos)
+        print(df)
         df.to_csv('pos.csv')
+        dfv = pd.DataFrame(self.vel)
+        dfv.to_csv('vel.csv')
