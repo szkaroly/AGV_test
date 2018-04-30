668832913a521a813d3168ca2ad1d12263829908
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-04-30 15:09:56 +0200

    updated datarecorder with sim logger

--- python/vrep/helloworld/dataRercorder.py
+++ python/vrep/helloworld/dataRercorder.py
@@ -10,6 +10,9 @@ class dataRecorder(object):
         self.posIndex = 0
         self.vel = {'index': [], 'v' : [], 'w':[], 'vr' :[], 'vl':[]}
         self.velIndex = 0
+        self.sim = {'index': [], 'v_c' : [], 'w_ang_c':[], 'w_ang_sim':[] , 'vr' :[], 'vl':[]}
+        self.simIndex = 0
+
 
     def recordPosition(self, x, y,th):
         ts = time.time()
@@ -27,8 +30,17 @@ class dataRecorder(object):
         self.vel['vr'].append(vr)
         self.vel['vl'].append(vl)
 
+    def recordSimData(self, v_c, w_ang_c, w_ang_sim, vr, vl):
+        self.simIndex = self.simIndex +1
+        self.sim['index'].append(self.simIndex)
+        self.sim['v_c'].append(v_c)
+        self.sim['w_ang_c'].append(w_ang_c)
+        self.sim['w_ang_sim'].append(w_ang_sim)
+        self.sim['vr'].append(vr)
+        self.sim['vl'].append(vl)
+
     def save(self):
         df = pd.DataFrame(self.pos)
-        df.to_csv('pos.csv')
-        dfv = pd.DataFrame(self.vel)
+        #df.to_csv('pos.csv')
+        dfv = pd.DataFrame(self.sim)
         dfv.to_csv('vel.csv')
