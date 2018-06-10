06ff20c5d9940068cb84c36e2d34387ed76314ba
Dániel Rácz <daniel.racz@irob.uni-obuda.hu>
2018-06-10 13:18:39 +0200

    renamed main package name to ginop_oe

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='ginop_control',
+setup(name='ginop_oe',
       version='0.2',
       description='Package that contains a set of algorithms and classes to move AGVs in VREP',
       url='-',
       author='Dániel Rácz',
       author_email='racz.daniel.93@gmail.com',
       license='MIT',
-      packages=['ginop_control', 'ginop_vrep', 'ginop_robots','vrep_norm'],
+      packages=['ginop_control', 'ginop_vrep', 'ginop_robots', 'vrep_norm'],
       install_requires=[
           'numpy',
           'pandas'
